3c180866a9bd64741f95e935687a17d101f290b6
Lucas Pereira Batista <lucas.batista9734@gmail.com>
2025-07-09 23:02:59 -0300

    feat: challenge completed

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("13996830085", "Nokia 2", "00131239012", "64");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Iphone iphone = new Iphone("13996830085", "Nokia 2", "00131239012", "64");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Skype");
